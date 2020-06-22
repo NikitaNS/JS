1ba86a9f7e713ddb95715bee7bfa98fdb70410e0
NikitaNS <nikitans1508@gmail.com>
2020-06-22 19:32:28 +0700

    initial js

--- 3-modul/main.js
+++
@@ -1,9 +0,0 @@
-function printHello(firstName, lastName) {
-    firstName = firstName || 'User';
-    lastName = lastName || 'User';
-    let fullName = firstName + ' ' + lastName
-    alert('Hello, ' + fullName);
-}
-
-printHello('asd', 'wer');
-
